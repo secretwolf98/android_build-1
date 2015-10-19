d11b682c4c112fbbb95df73487044f2de82882fa
D. Andrei Măceș <dmaces@nd.edu>
2015-10-19 15:43:24 -0400

    envsetup: Function to create remote to own forked repos
    Change-Id: Ia1ad8244e66a2c315479f671ffb8b64a3ff3a8ba

--- envsetup.sh
+++ envsetup.sh
@@ -1920,6 +1920,23 @@ function cmremote()
     echo You can now push to "cmremote".
 }
 
+function airmote()
+{
+    git remote rm airend 2> /dev/null
+    if [ ! -d .git ]
+    then
+        echo .git directory not found. Please run this from the root directory of the Android repository you wish to set up.
+    fi
+    PROJECT=`git config --get remote.github.projectname | sed s#CyanogenMod#airend#g`
+    if [ -z "$PROJECT" ]
+    then
+        echo Unable to find CM project, maybe an AOSP repo?
+        return 0
+    fi
+    git remote add airend git@github.com:$PROJECT.git
+    echo "Remote 'airend' created"
+}
+
 function aospremote()
 {
     git remote rm aosp 2> /dev/null
