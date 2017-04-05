06bef659c08f5e549e78815610c219b49798fb7e
D. Andrei Măceș <dmaces@nd.edu>
2017-04-05 14:58:19 -0400

    clang: Overridable RS_/LLVM_PREBUILTS & path checks
    Fall back to custom paths if upstream AOSP names are not found.
    
    Change-Id: I8c70f06d6520c53e7634b3e8d41ddfb324204b85

--- core/clang/versions.mk
+++ core/clang/versions.mk
@@ -1,5 +1,13 @@
 ## Clang/LLVM release versions.
 
-LLVM_RELEASE_VERSION := 3.8
+LLVM_RELEASE_VERSION ?= 3.8
 LLVM_PREBUILTS_VERSION ?= clang-2690385
 LLVM_PREBUILTS_BASE ?= prebuilts/clang/host
+
+pathver = $(if $(strip $(wildcard $(1)/$(BUILD_OS)-x86/$(2))),$(2),$(3))
+
+LLVM_RELEASE_VERSION := $(call pathver,\
+  $(LLVM_PREBUILTS_BASE),$(LLVM_RELEASE_VERSION),rt)
+
+LLVM_PREBUILTS_VERSION := $(call pathver,\
+  $(LLVM_PREBUILTS_BASE),$(LLVM_PREBUILTS_VERSION),cc)
